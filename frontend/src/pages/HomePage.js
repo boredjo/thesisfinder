46d2e01cf9b387937a421e0c94716203c1d1a219
Rayxz <ssf2channel@gmail.com>
2024-02-28 13:57:57 -0500

    Remove unnecessary files

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,8 @@
 // /frontend/src/pages/HomePage.js
 
 import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 const HomePage = () => {
   return (
