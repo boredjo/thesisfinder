93b58bb2abb1c09a440bff9c38e07fb331736f81
Rayxz <ssf2channel@gmail.com>
2024-02-28 12:56:41 -0500

    Update structure and README.md files

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,1 +1,13 @@
- 
+// /frontend/src/components/Footer.js
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; 2023 ThesisFinder</p>
+    </footer>
+  );
+};
+
+export default Footer; 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,1 +1,17 @@
- 
+ // /frontend/src/components/Header.js
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>ThesisFinder</h1>
+      <nav>
+        <a href="#">Log in</a>
+        <a href="#">Join for free</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
