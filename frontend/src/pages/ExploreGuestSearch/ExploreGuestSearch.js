4ab7bb32ba9fbfea6871bd5795392039558bfaf8
HarryS2496 <hselvaratnam23@gmail.com>
2024-03-08 19:21:01 -0500

    Removed unnecessary header and footer

--- frontend/src/pages/ExploreGuestSearch/ExploreGuestSearch.js
+++ frontend/src/pages/ExploreGuestSearch/ExploreGuestSearch.js
@@ -2,17 +2,12 @@
 // Explore page for those not logged in
 
 import React from 'react';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
 import 'styles/main.css';
 import 'styles/exploreguest.css';
 
 const ExploreGuestSearch = () => {
   return (
     <div>
-      <Header />
-
-      <Footer />
 
     </div>
   );
--- frontend/src/pages/ExploreSearch/ExploreSearch.js
+++ frontend/src/pages/ExploreSearch/ExploreSearch.js
@@ -2,8 +2,6 @@
 // This search page appears after typing a result into the search bar on either the login or home page.
 
 import React from 'react';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
 import '../../styles/main.css';
 import './explore-search.css';
 
