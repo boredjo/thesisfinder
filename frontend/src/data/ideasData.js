2810538382cd8f98497560df444b5412ba6aa515
Rayxz <ssf2channel@gmail.com>
2024-03-10 17:40:58 -0400

    Update idea data

--- frontend/src/data/ideasData.js
+++ frontend/src/data/ideasData.js
@@ -65,9 +65,97 @@ const ideas = [
       'Astrophysics',
     ],
     date: 'Aug 2023',
-    author: 'Isabella Rodriguez',
+    author: 'Emily Parker',
     authorImage: require('../assets/avatar1.png'),
-  }
+  },
+  {
+    title: "Exploring Multiverse Theories in Cosmology",
+    tags: [
+      'Multiverse', 
+      'Cosmology',
+      'Theoretical Physics',
+    ],
+    date: 'Jun 2022',
+    author: 'Emily Parker',
+    authorImage: require('../assets/avatar1.png'),
+  },
+  {
+    title: "Cosmic Microwave Background Anomalies: Unraveling Mysteries",
+    tags: [
+      'CMB Anomalies', 
+      'Cosmology',
+      'Observational Astronomy',
+    ],
+    date: 'Sep 2021',
+    author: 'Liam Taylor',
+    authorImage: require('../assets/avatar1.png'),
+  },
+  {
+    title: "Quantum Cosmology: Bridging Quantum Mechanics and the Cosmos",
+    tags: [
+      'Quantum Cosmology', 
+      'Quantum Mechanics',
+      'Theoretical Physics',
+    ],
+    date: 'Dec 2020',
+    author: 'Sophia Garcia',
+    authorImage: require('../assets/avatar1.png'),
+  },
+  {
+    title: "Cosmological Constant Debate: Observational Insights",
+    tags: [
+      'Cosmological Constant', 
+      'Dark Matter',
+      'Observational Cosmology',
+    ],
+    date: 'Mar 2023',
+    author: 'Daniel White',
+    authorImage: require('../assets/avatar1.png'),
+  },
+  {
+    title: "Inflationary Universe Models: Assessing Viability",
+    tags: [
+      'Inflationary Universe', 
+      'Comsology',
+      'Theoretical Physics',
+    ],
+    date: 'Jul 2022',
+    author: 'Daniel White',
+    authorImage: require('../assets/avatar1.png'),
+  },
+  {
+    title: "Cosmic String Theory: Investigating Potential Signatures",
+    tags: [
+      'Cosmic Strings', 
+      'String Theory',
+      'Theoretical Physics',
+    ],
+    date: 'Nov 2021',
+    author: 'Ava Turner',
+    authorImage: require('../assets/avatar1.png'),
+  },
+  {
+    title: "Holographic Cosmology: Applying AdS/CFT Correspondence",
+    tags: [
+      'Holographic Cosmology', 
+      'Ads/CFT',
+      'Theoretical Physics',
+    ],
+    date: 'Mar 2023',
+    author: 'Daniel White',
+    authorImage: require('../assets/avatar1.png'),
+  },
+  {
+    title: "Cosmic Neutrinos: Probing the Early Universe",
+    tags: [
+      'Cosmic Neutrinos', 
+      'Early Universe',
+      'Particle Physics',
+    ],
+    date: 'Oct 2023',
+    author: 'Isabella Reed',
+    authorImage: require('../assets/avatar1.png'),
+  },
 ];
 
   export default ideas;
