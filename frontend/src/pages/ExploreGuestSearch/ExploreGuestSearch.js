13c30587080e3cd0afae886858a87fec07bbe536
Rayxz <ssf2channel@gmail.com>
2024-03-10 15:43:36 -0400

    add ExploreGuestSearch to Routes

--- frontend/src/pages/ExploreGuestSearch/ExploreGuestSearch.js
+++ frontend/src/pages/ExploreGuestSearch/ExploreGuestSearch.js
@@ -1,9 +1,7 @@
-// /frontend/src/pages/ExploreGuestSearch.js
-// Explore page for those not logged in
-
 import React from 'react';
-import 'styles/main.css';
-import 'styles/exploreguest.css';
+
+import '../../styles/main.css';
+import './explore-guest-search.css';
 
 const ExploreGuestSearch = () => {
   return (
