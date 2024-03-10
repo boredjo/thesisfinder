b66ccae4e4bf67785ce2d21892d3965a1deac5f6
Rayxz <ssf2channel@gmail.com>
2024-03-10 16:09:20 -0400

    create ideasData.js for testing purposes

---
+++ frontend/src/data/ideasData.js
@@ -0,0 +1,62 @@
+const ideas = [
+    { 
+      title: 'Exploring Sustainable Urban Agriculture',
+      tags: [
+        'Urban Agriculture', 
+        'Sustainability', 
+        'Food Security',
+        'City Planning',
+      ],
+      date: 'Jan 2024',
+      author: 'Emma Thompson',
+      authorImage: require('../assets/avatar1.png'),
+    },
+    { 
+      title: 'Unlocking the Secrets of Dark Matter',
+      tags: [
+        'Astrophysics', 
+        'Dark Matter', 
+        'Cosmology', 
+        'Particle Physics'
+      ],
+      date: 'Jan 2024',
+      author: 'Adrian Martinez',
+      authorImage: require('../assets/avatar1.png'),
+    },
+    { 
+      title: 'Revolutionizing Renewable Energy Storage',
+      tags: [
+        'Renewable Energy', 
+        'Energy Storage',
+        'Sustainability',
+      ],
+      date: 'Jan 2024',
+      author: 'Olivia Johnson',
+      authorImage: require('../assets/avatar1.png'),
+    },
+    { 
+      title: 'Cognitive Enhancements through Neurofeedback',
+      tags: [
+        'Neurofeedback', 
+        'Brain Health',
+        'Psychology',
+      ],
+      date: 'Jan 2024',
+      author: 'Ethan Davis',
+      authorImage: require('../assets/avatar1.png'),
+    },
+    { 
+      title: 'Biodiversity Conversation in Urban Environments',
+      tags: [
+        'Biodiversity', 
+        'Conversation',
+        'Urban Ecology',
+        'Environmental Science',
+      ],
+      date: 'Jan 2024',
+      author: 'Isabella Rodriguez',
+      authorImage: require('../assets/avatar1.png'),
+    },
+  ];
+
+  export default ideas;
--- frontend/src/pages/ExploreGuestSearch/ExploreGuestSearch.js
+++ frontend/src/pages/ExploreGuestSearch/ExploreGuestSearch.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import FeaturesIdeas from '../../components/FeatureIdeas/FeatureIdeas';
+
+import ideas from '../../data/ideasData.js';
 
 import '../../styles/main.css';
 import './explore-guest-search.css';
 
 const ExploreGuestSearch = () => {
+  // Access the search query from the URL params using useParams
+  const { query } = useParams();
+
+  // Assuming 'ideas' is a global variable, you can use it here
+  const filteredIdeas = ideas.filter((idea) =>
+    idea.title.toLowerCase().includes(query.toLowerCase())
+  );
+
   return (
     <div>
-
+      <h2>Search Results for "{query}"</h2>
+      <FeaturesIdeas ideas={filteredIdeas} />
     </div>
   );
 };
 
-export default ExploreGuestSearch; 
+export default ExploreGuestSearch;
