9c989a139d7392b0d1c9fc924d510d2e61773aeb
Rayxz <ssf2channel@gmail.com>
2024-03-10 16:26:03 -0400

    Remove Search Button, Add header and description

--- frontend/src/pages/ExploreGuestSearch/ExploreGuestSearch.js
+++ frontend/src/pages/ExploreGuestSearch/ExploreGuestSearch.js
@@ -28,17 +28,27 @@ const ExploreGuestSearch = () => {
 
   return (
     <div>
-      <div className="search-bar">
+      <div className='header-info'>
+        <h2>Explore Research Ideas</h2>
+        <p>
+          Explore Research Ideas With a repository boasting a multitude of 
+          scientific insights, our platform is your gateway to a plethora 
+          of research ideas. Uncover a diverse collection of topics, 
+          connecting you with a thriving community of researchers. Utilize 
+          advanced search functionalities employing AND, OR, NOT, "" and 
+          () to tailor your exploration. Join us in accessing the world of 
+          science and fostering collaboration.
+        </p>
+      </div>
+      <div className="search-container">
         <input
+          id="search-bar"
           type="text"
           placeholder="Search Ideas"
           value={query}
           onKeyPress={handleKeyPress}
           onChange={(e) => setQuery(e.target.value)}
         />
-        <Link to={`/explore-guest-search/${query}`}>
-          <button>Search</button>
-        </Link>
       </div>
       <h2>Search Results for "{query}"</h2>
       <FeaturesIdeas ideas={filteredIdeas} />
