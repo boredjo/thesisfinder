0029c0ffa550b9c6bf0107b9cebb45fceb2286bd
jo <aacount@jokaendler.eu>
2024-03-12 23:45:08 -0400

    update post user to parse middleware

--- backend/src/routes/user.py
+++ backend/src/routes/user.py
@@ -15,21 +15,28 @@ def post_user():
 
     # parse incoming data
     try:
-        data = request.get_json()
+        data = request.environ['parsed_data']
         new_user = User(data['user'], data['first_name'], data['last_name'], data['country'], data['email'], data['password'])
-    except:
+    except Exception as e:
+        request.environ['logging'].error(e, 'routes/user.py - post_user() - parse new user data')
         return Response(u'Could process the request', mimetype= 'text/plain', status=422)
     
     # update database
     if user.name == new_user.name: # updateing user info
         if new_user.emailTaken(request.environ['cursor']):
+            request.environ['logging'].message("POST_USER", f'{new_user.email} is already taken')
             return Response(u'Username or Email is already taken', mimetype= 'text/plain', status=422)
         user.update(new_user, request.environ['cursor'])
+        request.environ['logging'].message("POST_USER", f'{new_user.name} updated')
         return Response(u'updating user info', mimetype= 'text/plain', status=200)
+    
     elif user.isAnon(): # create new user
         if new_user.nameTaken(request.environ['cursor']):
+            request.environ['logging'].message("POST_USER", f'{new_user.name} or {new_user.email} is already taken')
             return Response(u'Username or Email is already taken', mimetype= 'text/plain', status=422)
         new_user.store(request.environ['cursor'])
+        request.environ['logging'].message("POST_USER", f'{new_user.name} created')
         return Response(u'create new user', mimetype= 'text/plain', status=200)
     else:
+        request.environ['logging'].message("POST_USER", 'auth fail')
         return Response(u'You are not authorized to do this action', mimetype= 'text/plain', status=401)
