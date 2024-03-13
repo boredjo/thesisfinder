b68e262b0fa12273ac60e0299a5153fdd2176d98
jo <aacount@jokaendler.eu>
2024-03-12 22:35:38 -0400

    simplified logging

--- backend/src/utils/logs.py
+++ backend/src/utils/logs.py
@@ -1,4 +1,3 @@
-from werkzeug.wrappers import Request, Response, ResponseStream
 from datetime import datetime
 
 # define colors
@@ -11,20 +10,25 @@ BLUE = '\033[94m'
 PURPLE = '\033[95m'
 LIGHTBLUE = '\033[96m'
 
-def log_error(e, location_in_code = 'unspecified'):
-    print(f'{GRAY}[{datetime.now().strftime("%H:%M:%S")}]{RED} ERROR({type(e)}):{ENDC} {e} {GRAY}[location: {location_in_code}]{ENDC}')
-
 def log_info(message, location_in_code = 'unspecified'):
     print(f'{GRAY}[{datetime.now().strftime("%H:%M:%S")}]{BLUE} INFO:{ENDC} {message} {GRAY}[location: {location_in_code}]{ENDC}') 
 
-class log_middleware():
-    def __init__(self, app):
-        self.app = app
+def log_error(self, e, location_in_code = 'unspecified'):
+        print(f'{GRAY}[{datetime.now().strftime("%H:%M:%S")}]{RED} ERROR({type(e)}):{ENDC} {e} {GRAY}[location: {location_in_code}]{ENDC}')
 
-    def __call__(self, environ, start_response):
-        request = Request(environ)
-        print(f'{GRAY}[{datetime.now().strftime("%H:%M:%S")}]{YELLOW} REQUEST-HEADER:{ENDC} {list(request.headers)} {ENDC}')
-        
-        print(f'{GRAY}[{datetime.now().strftime("%H:%M:%S")}]{YELLOW} REQUEST-BODY:{ENDC} {request.get_json()} {ENDC}')
+class Logger:
+    Request_Count = 0
 
-        return res(environ, start_response)
+    def __init__(self):
+        Logger.Request_Count += 1
+        self.request_id = Logger.Request_Count
+        self.message('RECEIVED')
+
+    def error(self, e, location_in_code = 'unspecified'):
+        print(f'{GRAY}[{datetime.now().strftime("%H:%M:%S")}] {LIGHTBLUE}REQUEST{self.request_id :06d}{GRAY}-{RED}ERROR({type(e)}){ENDC}: {e} {GRAY}[location: {location_in_code}]{ENDC}')
+
+    def info(self, message, location_in_code = 'unspecified'):
+        print(f'{GRAY}[{datetime.now().strftime("%H:%M:%S")}] {LIGHTBLUE}REQUEST{self.request_id :06d}{GRAY}-{BLUE}INFO{ENDC}: {message} {GRAY}[location: {location_in_code}]{ENDC}') 
+
+    def message(self, type, message=''):
+        print(f'{GRAY}[{datetime.now().strftime("%H:%M:%S")}] {LIGHTBLUE}REQUEST{self.request_id :06d}{GRAY}-{YELLOW}{type}{ENDC}{"" if len(message) == 0 else ": "}{message}{ENDC}')
