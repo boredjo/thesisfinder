f92efa50cc48b68ac38b5ed40a7e51357907e3a7
jo <aacount@jokaendler.eu>
2024-03-12 22:36:15 -0400

    restructered connection into middleware

--- backend/src/utils/db.py
+++ backend/src/utils/db.py
@@ -1,33 +1,42 @@
+from werkzeug.wrappers import Response
+from dotenv import load_dotenv
 import mysql.connector
 import sys
 import os
-from dotenv import load_dotenv
+
 from utils.logs import log_error, log_info
 
-# Load variables from .env file
-load_dotenv(".env")
-# Connect to MariaDB Platform
+class mysql_middleware():
+    def __init__(self, app):
+        load_dotenv(".env")
+        try:
+            log_info(f"Connecting to MYSQL at {os.environ['DB_USER']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}", 'db.py')
+            # Connect to MySQL
+            self.connection = mysql.connector.connect(
+                user=os.environ['DB_USER'],
+                password=os.environ['DB_PASSWORD'],
+                host=os.environ['DB_HOST'],
+                port=int(os.environ['DB_PORT']),
+                database=os.environ['DB_DATABASE'],
+                autocommit=True
+            )
+            log_info(f'Successfully Connected to MYSQL', 'db.py')
+            
+        except mysql.connector.Error as e:
+            log_error(e, 'db.py - establish connection')
+            sys.exit(1)
+        self.app = app
 
-try:
-    log_info(f"Connecting to MYSQL at {os.environ['DB_USER']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}", 'db.py')
-    # Connect to MySQL
-    conn = mysql.connector.connect(
-        user=os.environ['DB_USER'],
-        password=os.environ['DB_PASSWORD'],
-        host=os.environ['DB_HOST'],
-        port=int(os.environ['DB_PORT']),
-        database=os.environ['DB_DATABASE'],
-        autocommit=True
-    )
-    log_info(f'Successfully Connected to MYSQL', 'db.py')
+    def __call__(self, environ, start_response):
+        if not self.connection.is_connected():
+            environ['logger'].info('Reconecting to DB', 'db.py')
+            self.connection.reconnect(3)
+        
+        if not self.connection.is_connected():
+            environ['logger'].error('Cannot connect to DB', 'db.py')
+            res = Response(u'Internal Server Error', mimetype= 'text/plain', status=500)
+            return res(environ, start_response)
 
-
-    # If the connection is successful, proceed with further operations
-    
-except mysql.connector.Error as e:
-    log_error(e, 'db.py - establish connection')
-    sys.exit(1)
-
-
-# # Get Cursor
-# cur = conn.cursor(buffered=True)
+        environ['cursor'] = self.connection.cursor(buffered=True)
+        environ['logger'].message('MYSQL', 'attached cursor')
+        return self.app(environ, start_response)
