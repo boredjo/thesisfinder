5753d098cf48485d804c12bf38236178e7b66357
jo <aacount@jokaendler.eu>
2024-03-19 11:36:07 -0400

    allowed image/png for profile pictures

--- backend/src/utils/parse.py
+++ backend/src/utils/parse.py
@@ -1,7 +1,21 @@
-from werkzeug.wrappers import Request, Response, ResponseStream
+from werkzeug.wrappers import Request, Response
 from werkzeug.exceptions import BadRequest
 from utils.logs import Logger
 
+# onyl use first 4 characters
+JSON_PATHS = [
+    "/use", # /user
+    "/log", # /login
+]
+
+IMAGE_PATHS = [
+    "/pro", # /profilepicture
+]
+
+ALLOWED_EXTENSIONS = set(['png'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class parse_middleware():
     
@@ -12,32 +26,45 @@ class parse_middleware():
         request = Request(environ)
         if request.path.startswith("/doc"): # pass trough for the documentation
             return self.app(environ, start_response)
-        
 
         # establish logger
         if not 'logger' in environ.keys():
             environ['logger'] = Logger()
 
         environ['logger'].message('HEADER', list(request.headers))
-
         # do not parse GET request
-        if request.method == 'GET':
+        if request.method in ['GET', 'DELETE']:
             return self.app(environ, start_response)
-
-        # parse request body
-        if 'Content-Type' in request.headers.keys() and request.headers['Content-Type'] == 'application/json':
-            try:
-                environ['parsed_data'] = request.get_json()
-                environ['logger'].message('BODY', environ['parsed_data'])
-                return self.app(environ, start_response)
-            except BadRequest as e:
-                environ['logger'].error(e, 'parse.py - casting to json')
-                res = Response(u'Could process the request', mimetype= 'text/plain', status=422)
-                return res(environ, start_response)
-            
-        # block non json calls
-        environ['logger'].message('NOT JSON')
-        res = Response(u'This API only processes application/json content', mimetype= 'text/plain', status=422)
-        return res(environ, start_response)
-    
         
+        if request.path[:4] in JSON_PATHS: 
+            if 'Content-Type' in request.headers.keys() and request.headers['Content-Type'] == 'application/json':
+                try:
+                    environ['parsed_data'] = request.get_json()
+                    environ['logger'].message('BODY', environ['parsed_data'])
+                    return self.app(environ, start_response)
+                except BadRequest as e:
+                    environ['logger'].error(e, 'parse.py - casting to json')
+                    res = Response(u"Couldn't process the request", mimetype= 'text/plain', status=422)
+                    return res(environ, start_response)
+                
+            # block non json calls
+            environ['logger'].message('NOT JSON')
+            res = Response(u'This endpoint only processes application/json content', mimetype= 'text/plain', status=422)
+            return res(environ, start_response)
+
+
+        elif request.path[:4] in IMAGE_PATHS:
+            if 'Content-Type' in request.headers.keys() and request.headers['Content-Type'] == 'image/png':
+                environ['parsed_data'] = request.get_data()
+                environ['logger'].message('BODY', 'parsed binary')
+                return self.app(environ, start_response)
+            # block non png calls
+            environ['logger'].message('NOT PNG')
+            res = Response(u'This endpoint only processes image/png content', mimetype= 'text/plain', status=422)
+            return res(environ, start_response)
+                
+        environ['logger'].message("PARSE", f'unkown path {request.path}')
+        res = Response(u"Couldn't process the request", mimetype= 'text/plain', status=422)
+        return res(environ, start_response)
+
+ 
