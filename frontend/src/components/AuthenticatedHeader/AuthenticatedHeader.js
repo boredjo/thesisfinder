1f4f81efa93556854c1923467dc55d9efe0c7cd7
Rayxz <ssf2channel@gmail.com>
2024-03-22 00:24:41 -0400

    add modal for submit in header

--- frontend/src/components/AuthenticatedHeader/AuthenticatedHeader.js
+++ frontend/src/components/AuthenticatedHeader/AuthenticatedHeader.js
@@ -12,6 +12,7 @@ const AuthenticatedHeader = () => {
   const navigate = useNavigate();
   const [avatarImage, setAvatarImage] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isSubmitModalOpen, setIsSubmitModalOpen] = useState(false);
 
   useEffect(() => {
     const savedAvatarImage = localStorage.getItem('avatarImage');
@@ -28,73 +29,76 @@ const AuthenticatedHeader = () => {
     setIsModalOpen(false);
   };
 
+  const openSubmitModal = () => {
+    setIsSubmitModalOpen(true);
+  };
+
+  const closeSubmitModal = () => {
+    setIsSubmitModalOpen(false);
+  };
+
   const handleLogout = () => {
     clearAuthenticatedUser();
     navigate("/");
   };
 
   return (
-    // <header className="main-header">
-    //     <div className="left-section">
-    //       <img className="thesisfinder-logo" src={require('../../assets/thesisfinderlogo.png')} alt="Thesis-Finder-Logo" />
-    //       <button className="left-section-button">Home</button>
-    //       <div className="button-selector"></div>
-    //       <button className="left-section-button">Ideas</button>
-    //     </div>
-    //     <div className="middle-section">
-    //       <input className="main-header-search" type="text" placeholder="Search for research ideas, sponsorships, people, etc." />
-    //     </div>
-    //     <div className="right-section">
-    //       <img className="dark-mode-button" src={require('../../assets/darkmodeimage.png')} alt="Dark Mode" />
-    //       <img className="notification-button" src={require('../../assets/notificationimage.png')} alt="Notification" />
-    //       <img className="account-button" src={require('../../assets/avatar1.png')} alt="User Account" />
-    //       <button className="header-submit-button">Submit</button>
-    //     </div>
-    //   </header>
-
     <header className="main-header">
-    <div className="left-section">
-      <img className="thesisfinder-logo" src={require('../../assets/thesisfinderlogo.png')} alt="Thesis-Finder-Logo" />
-      <button className="left-section-button" onClick={() => navigate('/')}>Home</button>
-      {/* <div className="button-selector"></div> */}
-      <button className="left-section-button" onClick={() => navigate('/explore-guest-search/e')}>Ideas</button>
-    </div>
-    <div>
-      <input className="main-header-search" type="text" placeholder="Search for research ideas, sponsorships, people, etc." />
-    </div>
-    <div>
-      <button  onClick={() => {/* Handle dark mode button click */}}>
-        <img className="dark-mode-button" src={require('../../assets/darkmodeimage.png')} alt="Dark Mode" />
-      </button>
-      <button onClick={() => {/* Handle notification button click */}}>
-      <img className="notification-button" src={require('../../assets/notificationimage.png')} alt="Notification" />
-      </button>
-      <button onClick={openModal}>
-        {avatarImage ? (
-          <img src={avatarImage} alt="User Account" />
-        ) : (
-          <img className="account-button" src={require('../../assets/avatar1.png')} alt="User Account" />
-        )}
-      </button>
-      <button className="header-submit-button">Submit</button>
-    </div>
-    {/* Render the ProfileOptionsModal if isModalOpen is true */}
-    {isModalOpen && (
-      <div className="modal-overlay" onClick={closeModal}>
-        <div className="modal-content profile-options-modal" onClick={(e) => e.stopPropagation()}>
-          <div className="modal-header">
-            <h2>Profile Options</h2>
-            <button onClick={closeModal} className="close-button">X</button>
-          </div>
-          <div className="modal-body">
-            <button onClick={() => navigate('/account')}>Your Profile</button>
-            <button onClick={handleLogout}>Logout</button>
+      <div className="left-section">
+        <img className="thesisfinder-logo" src={require('../../assets/thesisfinderlogo.png')} alt="Thesis-Finder-Logo" />
+        <button className="left-section-button" onClick={() => navigate('/')}>Home</button>
+        <button className="left-section-button" onClick={() => navigate('/explore-guest-search/e')}>Ideas</button>
+      </div>
+      <div>
+        <input className="main-header-search" type="text" placeholder="Search for research ideas, sponsorships, people, etc." />
+      </div>
+      <div>
+        <button onClick={() => {/* Handle dark mode button click */}}>
+          <img className="dark-mode-button" src={require('../../assets/darkmodeimage.png')} alt="Dark Mode" />
+        </button>
+        <button onClick={() => {/* Handle notification button click */}}>
+          <img className="notification-button" src={require('../../assets/notificationimage.png')} alt="Notification" />
+        </button>
+        <button onClick={openModal}>
+          {avatarImage ? (
+            <img src={avatarImage} alt="User Account" />
+          ) : (
+            <img className="account-button" src={require('../../assets/avatar1.png')} alt="User Account" />
+          )}
+        </button>
+        <button className="header-submit-button" onClick={openSubmitModal}>Submit</button>
+      </div>
+      {/* Render the ProfileOptionsModal if isModalOpen is true */}
+      {isModalOpen && (
+        <div className="modal-overlay" onClick={closeModal}>
+          <div className="modal-content profile-options-modal" onClick={(e) => e.stopPropagation()}>
+            <div className="modal-header">
+              <h2>Profile Options</h2>
+              <button onClick={closeModal} className="close-button">X</button>
+            </div>
+            <div className="modal-body">
+              <button onClick={() => navigate('/account')}>Your Profile</button>
+              <button onClick={handleLogout}>Logout</button>
+            </div>
           </div>
         </div>
-      </div>
-    )}
-  </header>
-
+      )}
+      {/* Render the SubmitModal if isSubmitModalOpen is true */}
+      {isSubmitModalOpen && (
+        <div className="modal-overlay" onClick={closeSubmitModal}>
+          <div className="modal-content submit-modal" onClick={(e) => e.stopPropagation()}>
+            <div className="modal-header">
+              <h2>Submit Options</h2>
+              <button onClick={closeSubmitModal} className="close-button">X</button>
+            </div>
+            <div className="modal-body">
+              <button onClick={() => {/* Handle "Submit Research Idea" button click */}}>Submit Research Idea</button>
+              <button onClick={() => {/* Handle "Your Research Papers" button click */}}>Your Research Papers</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </header>
   );
 };
 
