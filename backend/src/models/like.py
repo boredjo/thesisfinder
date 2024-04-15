c1b7e3ae1833fbf0d8066913e2d1723a3693c452
jo <aacount@jokaendler.eu>
2024-04-15 01:49:50 -0400

    implemented likes

--- backend/src/models/like.py
+++ backend/src/models/like.py
@@ -1,12 +1,43 @@
 from flask import jsonify
 
+from models.idea import Idea
+
 class Like:
 
-    def get_status(cursor, user, idea):
-        return False
+    def get_status(cursor, user:str, idea:str):
+        cursor.execute(
+            """
+            SELECT get_likes_function(%s, %s);
+            """
+            , [user, idea]
+        )
+        row = cursor.fetchone()
+        return row[0] == 1
 
-    def like_idea(cursor, user, idea):
-        return Like.get_status(cursor, user, idea)
+    def like_idea(cursor, user:str, idea:str):
+        cursor.execute(
+            """
+            SELECT update_like_function(%s, %s);
+            """
+            , [user, idea]
+        )
+        row = cursor.fetchone()
+        return row[0] == 1
 
-    def get_user_likes(cursor, user, idea):
-        return []
+    def get_user_likes(cursor, user:str):
+        data = []
+        cursor.execute(
+            """
+            SELECT Ideas.title, Ideas.author, Ideas.tag1, Ideas.tag2, Ideas.tag3, Ideas.tag4, Ideas.tag5, Ideas.date_posted, Ideas.hash 
+            FROM Likes INNER JOIN Ideas  ON Ideas.hash = Likes.idea
+            WHERE Likes.user = %s;
+            """
+            , [user]
+        )
+        rows = cursor.fetchall()
+        for row in rows:
+            tags = list(row[2:7])
+            idea = Idea(row[0], tags, row[1], "", id=row[8])
+            idea.date_posted = str(row[7])
+            data.append(idea)
+        return data
--- backend/src/routes/like.py
+++ backend/src/routes/like.py
@@ -1,6 +1,5 @@
 from werkzeug.wrappers import Response
 from flask import Blueprint, request, jsonify
-import mysql.connector
 from mysql.connector import cursor
 
 from utils.db import util_db
@@ -11,29 +10,15 @@ from models.like import Like
 from models.user import User
 
 
-claim_blueprint = Blueprint('like', __name__)
+like_blueprint = Blueprint('like', __name__)
 
-@claim_blueprint.route('/ ', methods=['POST'])
-@util_db()
-@util_auth()
-@parse_json(['idea'])
-def like_idea(data, cursor:cursor, user:User):
-    logger:Logger = request.environ['logger']
-    try:
-        status = Like.change_idea(cursor, user, data['idea'])
-    except Exception as err:
-        logger.error(err, 'routes/like.py - like_idea()) - like idea')
-        status = False
-
-    return jsonify(like=status)
-
-@claim_blueprint.route('/user/', methods=['GET'])
+@like_blueprint.route('/user/', methods=['GET'])
 @util_db()
 @util_auth()
 def get_user_likes(cursor:cursor, user:User):
     logger:Logger = request.environ['logger']   
     try:
-        data = Like.get_liks(cursor, user)
+        data = Like.get_user_likes(cursor, user.name)
     except Exception as err:
         logger.error(err, 'routes/like.py - get_user_likes() - find likes')
         data = []
@@ -49,15 +34,29 @@ def get_user_likes(cursor:cursor, user:User):
         for idea in data
     ])
 
-@claim_blueprint.route('/<path:ideaid>', methods=['GET'])
+
+@like_blueprint.route('/', methods=['POST'])
+@util_db()
+@util_auth()
+@parse_json(['idea'])
+def like_idea(data, cursor:cursor, user:User):
+    logger:Logger = request.environ['logger']
+    try:
+        status = Like.like_idea(cursor, user.name, data['idea'])
+    except Exception as err:
+        logger.error(err, 'routes/like.py - like_idea() - like idea')
+        status = False
+
+    return jsonify(like=status)
+
+@like_blueprint.route('/<path:ideaid>', methods=['GET'])
 @util_db()
 @util_auth()
 def get_like_status(cursor:cursor, user:User, ideaid):
     logger:Logger = request.environ['logger']
     try:
-        status = Like.get_status(cursor, user, ideaid)
+        status = Like.get_status(cursor, user.name, ideaid)
     except Exception as err:
         logger.error(err, 'routes/like.py - get_like_status()) - get status')
         status = False
-
     return jsonify(like=status)
