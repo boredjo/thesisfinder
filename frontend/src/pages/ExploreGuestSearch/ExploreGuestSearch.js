5f33d31226b6aec2a2875c7f162f5edf033ac6a5
Rayxz <ssf2channel@gmail.com>
2024-04-16 03:22:51 -0400

    update

--- frontend/src/pages/ExploreGuestSearch/ExploreGuestSearch.js
+++ frontend/src/pages/ExploreGuestSearch/ExploreGuestSearch.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import FeaturesIdeas from '../../components/FeatureIdeas/FeatureIdeas';
+import LoadingIndicator from '../../components/LoadingIndicator/LoadingIndicator'; // Import LoadingIndicator component
 import { getFeaturedIdeas } from '../../utils/api';
 import '../../styles/main.css';
 import './explore-guest-search.css';
@@ -16,6 +17,7 @@ const ExploreGuestSearch = () => {
   const [filteredIdeas, setFilteredIdeas] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setItemsPerPage] = useState(5); // Adjust the number of items per page
+  const [loading, setLoading] = useState(true); // State to track loading state
 
   useEffect(() => {
     const fetchFeaturedIdeas = async () => {
@@ -24,6 +26,8 @@ const ExploreGuestSearch = () => {
         setIdeas(response.ideas || []); // Update state with fetched ideas
       } catch (error) {
         console.error('Error fetching featured ideas:', error);
+      } finally {
+        setLoading(false); // Set loading state to false when request completes
       }
     };
 
@@ -79,20 +83,26 @@ const ExploreGuestSearch = () => {
           defaultValue={query}
         />
       </div>
-      <hr className="section-divider" />
-      <h2>Showing results for "{query}"</h2>
-      <FeaturesIdeas ideas={currentIdeasToShow} />
-      <div className="pagination">
-        {pageNumbers.map((number) => (
-          <span
-            key={number}
-            onClick={() => handlePageChange(number)}
-            className={number === currentPage ? 'active' : ''}
-          >
-            {number}
-          </span>
-        ))}
-      </div>
+      {loading ? ( // Show LoadingIndicator while loading
+        <LoadingIndicator />
+      ) : (
+        <>
+          <hr className="section-divider" />
+          <h2>Showing results for "{query}"</h2>
+          <FeaturesIdeas ideas={currentIdeasToShow} />
+          <div className="pagination">
+            {pageNumbers.map((number) => (
+              <span
+                key={number}
+                onClick={() => handlePageChange(number)}
+                className={number === currentPage ? 'active' : ''}
+              >
+                {number}
+              </span>
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
