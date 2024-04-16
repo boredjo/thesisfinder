a483c59d6e2dd707bc330641c0caac3552091128
Rayxz <ssf2channel@gmail.com>
2024-04-16 02:56:29 -0400

    update

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -8,30 +8,51 @@ import './header.css';
 const Header = () => {
   const [showLoginModal, setShowLoginModal] = useState(false);
   const authenticatedUser = getAuthenticatedUser();
-  const navigate = useNavigate();
+  const [searchQuery, setSearchQuery] = useState('');
+  const navigate = useNavigate()
 
   const handleLoginClick = () => {
     setShowLoginModal(true);
   };
 
-  const handleLogoutClick = () => {
-    clearAuthenticatedUser();
-    navigate('/');
-  };
+  // const handleLogoutClick = () => {
+  //   clearAuthenticatedUser();
+  //   navigate('/');
+  // };
 
   const handleLoginModalClose = () => {
     setShowLoginModal(false);
   };
 
+  const handleSearch = (event) => {
+    if (event.key === 'Enter') {
+      navigate(`/explore-guest-search?query=${searchQuery}`);
+    }
+  };
+
   return (
-    <header>
-      <Link id='header-title' to="/">ThesisFinder</Link>
-      <nav>
-          <button id='login-button' onClick={handleLoginClick}>Log in</button>
-          <Link id='signup-link' to="/signup">Join for free</Link>
-      </nav>
-      
-      <LoginModal show={showLoginModal} handleClose={handleLoginModalClose} />
+    <header className="main-header">
+      <div className="left-section">
+        <Link id='header-title' to="/">ThesisFinder</Link>
+      </div>
+      <div className='middle-section'>
+        <input
+          className="main-header-search"
+          type="text"
+          placeholder="Search for research ideas, sponsorships, people, etc."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          onKeyPress={handleSearch}
+        />
+      </div>
+      <div className='right-section'>
+        <nav>
+            <button id='login-button' onClick={handleLoginClick}>Log in</button>
+            <Link id='signup-link' to="/signup">Join for free</Link>
+        </nav>
+        <LoginModal show={showLoginModal} handleClose={handleLoginModalClose} />
+      </div>
+    
     </header>
   );
 };
