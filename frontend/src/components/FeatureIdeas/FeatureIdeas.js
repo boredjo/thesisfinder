dc329e56617f87c5395237a4b499147b8a7fdb4f
Rayxz <ssf2channel@gmail.com>
2024-04-18 16:13:20 -0400

    date posted

--- frontend/src/components/FeatureIdeas/FeatureIdeas.js
+++ frontend/src/components/FeatureIdeas/FeatureIdeas.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import TimeAgo from 'react-time-ago';
 import './feature-ideas.css';
+import ReactTimeAgo from 'react-time-ago';
 
 const FeaturesIdeas = ({ ideas }) => {
   return (
     <div className="features-ideas">
       {ideas.map((idea) => {
         const filteredTags = idea.tags.filter(tag => tag !== null);
-        console.log("https://data.thesisfinder.com/profilepicture/" + idea.author)
         return (
           <div key={idea.id} className="idea-container">
             {/* Link each idea title to the post page */}
@@ -19,15 +20,16 @@ const FeaturesIdeas = ({ ideas }) => {
                 </div>
               ))}
             </div>
-            <p className="date">{idea.date_posted}</p> {/* Use date_posted instead of date */}
             <div className="author-container">
               <img
-                src={"https://data.thesisfinder.com/profilepicture/" + idea.author}
+                src={`https://data.thesisfinder.com/profilepicture/${idea.author}`}
                 alt={`${idea.author}'s profile`}
                 className="author-image"
               />
               <p className="author">{idea.author}</p>
             </div>
+            {/* Pass the locale identifier string directly to TimeAgo component */}
+            <p className="date">Date: <ReactTimeAgo date={idea.date_posted} locale="en-US"/></p>
           </div>
         );
       })}
